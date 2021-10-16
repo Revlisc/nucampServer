5f9415f05ceb542ae521f8a52c056744833cad29
dales <dalesimmons0210@gmail.com>
2021-10-16 15:36:03 -0700

    week 4 workshop

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Partner = require('../models/partner');
-
+const authenticate = require('../authenticate');
 const partnerRouter = express.Router();
 
 partnerRouter.route('/')
@@ -13,7 +13,7 @@ partnerRouter.route('/')
     })
     .catch(err => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     partner.create(req.body)
     .then(partner => {
         console.log('partner Created ', partner);
@@ -27,7 +27,7 @@ partnerRouter.route('/')
     res.statusCode = 403;
     res.end('PUT operation not supported on /partners');
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     partner.deleteMany()
     .then(response => {
         res.statusCode = 200;
@@ -35,6 +35,42 @@ partnerRouter.route('/')
         res.json(response);
     })
     .catch(err => next(err));
+});
+
+partnerRouter.route('/:partnerId')
+    .get((req, res, next) => {
+        Partner.findById(req.params.partnerId)
+        .populate('comments.author')
+        .then(partner => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(partner);
+        })
+        .catch(err => next(err));
+    })
+    .post(authenticate.verifyUser, (req, res) => {
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /partners/${req.params.partnerId}`);
+    })
+    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Partner.findByIdAndUpdate(req.params.partnerId, {
+            $set: req.body
+        }, { new: true })
+            .then(partner => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(partner);
+            })
+            .catch(err => next(err));
+    })
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Partner.findByIdAndDelete(req.params.partnerId)
+            .then(response => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            })
+            .catch(err => next(err));
 });
 
 partnerRouter.route('/:partnerId/comments')
--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Promotion = require('../models/promotion');
-
+const authenticate = require('../authenticate');
 const promotionRouter = express.Router();
 
 promotionRouter.route('/')
@@ -13,7 +13,7 @@ promotionRouter.route('/')
     })
     .catch(err => next(err));
 })
-.post((req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     promotion.create(req.body)
     .then(promotion => {
         console.log('promotion Created ', promotion);
@@ -27,7 +27,7 @@ promotionRouter.route('/')
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
 })
-.delete((req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     promotion.deleteMany()
     .then(response => {
         res.statusCode = 200;
@@ -35,6 +35,42 @@ promotionRouter.route('/')
         res.json(response);
     })
     .catch(err => next(err));
+});
+
+promotionRouter.route('/:promotionId')
+    .get((req, res, next) => {
+        Promotion.findById(req.params.promotionId)
+        .populate('comments.author')
+        .then(promotion => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(promotion);
+        })
+        .catch(err => next(err));
+    })
+    .post(authenticate.verifyUser, (req, res) => {
+        res.statusCode = 403;
+        res.end(`POST operation not supported on /promotions/${req.params.promotionId}`);
+    })
+    .put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Promotion.findByIdAndUpdate(req.params.promotionId, {
+            $set: req.body
+        }, { new: true })
+            .then(promotion => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(promotion);
+            })
+            .catch(err => next(err));
+    })
+    .delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+        Promotion.findByIdAndDelete(req.params.promotionId)
+            .then(response => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json(response);
+            })
+            .catch(err => next(err));
 });
 
 promotionRouter.route('/:promotionId/comments')
