1c22d1bc404aedce6e56fbbb6bf0b9ebfb2e6ab1
dales <dalesimmons0210@gmail.com>
2021-10-20 22:55:51 -0700

    passport facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '578326163450780',
+        clientSecret: 'cfc250b84ca65578d1a88a10d297bd85'
+    }
 }
